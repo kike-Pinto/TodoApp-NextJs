fddf120ddf0ef4676312de358dd6256028e81ff1
kike-pinto <cripintort@gmail.com>
2024-10-10 04:12:34 -0300

    eliminar el use

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 'use client'
 
 import Todo from '@/Components/Todo'
-import { use, useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { ToastContainer, toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import axios from 'axios'
